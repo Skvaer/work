8717b0c9ccb72684e64b6765a74f5d01dcf70f96
Skvaer <skvaer76@gmail.com>
2019-03-12 14:50:00 +0200

    Добавили функции в скрипт

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 (function () {
     'use strict';
-    var money = +prompt("Какой бюджет на месяц?", ''),
+    let money, time;
+
+    function start() {
+        money = +prompt("Какой бюджет на месяц?", '');
         time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+        while (isNaN(money) || money == '' || money == null) {
+            money = +prompt("Какой бюджет на месяц?", '');
+        }
+    }
+    start();
     var appData = {
         budget: money,
         timeData: time,
         expenses: {},
         optionalExpenses: {},
         income: [],
-        saving: false
+        saving: true
     };
 
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-            b = prompt("Во сколько обойдется?", '');
-        if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
-            a != '' && b != '' && a.length < 50) {
-            appData.expenses[a] = b;
-        } else {
-            alert("Введите правильные данные повторно!");
-            i--;
+    function chooseExpenses() {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+                b = prompt("Во сколько обойдется?", '');
+            if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+                a != '' && b != '' && a.length < 50) {
+                appData.expenses[a] = b;
+            } else {
+                alert("Введите правильные данные повторно!");
+                i--;
+            }
         }
     }
-    appData.moneyPerDay = appData.budget / 30;
+    chooseExpenses();
+
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
     alert("Ежедневний бюджет становит: " + appData.moneyPerDay);
     console.log(appData);
 
@@ -34,5 +47,17 @@
         console.log("Високий уровеень достатка");
     } else {
         console.log("Произошла ошибка");
-    }    
+    }
+
+
+    function checkSaving() {
+        if (appData.saving == true) {
+            let save = +prompt("Какова сума накоплений?", ""),
+                persent = +prompt("Под какой процент?", "");
+            appData.monthIncome = save / 100 / 12 * persent;
+            alert("Доход за месяц с вашего депозита: " + appData.monthIncome.toFixed());
+        }
+    }
+
+    checkSaving();
 }());
