7c01b7d0192e3e5b3f2ce09b0d341c44ee53615c
Skvaer <skvaer76@gmail.com>
2019-03-12 12:46:53 +0200

    Добавил цикл для опроса и вывод уровня достатка

--- script.js
+++ script.js
@@ -10,13 +10,29 @@
         income: [],
         saving: false
     };
-    let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-        a2 = prompt("Во сколько обойдется?", ''),
-        a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-        a4 = prompt("Во сколько обойдется?", '');
-    appData.expenses.a1 = a2;
-    appData.expenses.a3 = a4;
 
-    alert(appData.budget/30);
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt("Во сколько обойдется?", '');
+        if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+            a != '' && b != '' && a.length < 50) {
+            appData.expenses[a] = b;
+        } else {
+            alert("Введите правильные данные повторно!");
+            i--;
+        }
+    }
+    appData.moneyPerDay = appData.budget / 30;
+    alert("Ежедневний бюджет становит: " + appData.moneyPerDay);
     console.log(appData);
+
+    if (appData.moneyPerDay < 100) {
+        console.log("Минимальний уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Високий уровеень достатка");
+    } else {
+        console.log("Произошла ошибка");
+    }    
 }());
